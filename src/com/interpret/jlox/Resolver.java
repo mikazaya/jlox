6ab2acabfe7690cdd0f728adfe7dfa9466f2c72d
shourya <shourya.s2012@gmail.com>
2024-06-20 09:34:45 +0530

    the

--- src/com/interpret/jlox/Resolver.java
+++ src/com/interpret/jlox/Resolver.java
@@ -1,5 +1,48 @@
 package com.interpret.jlox;
 
-public class Resolver {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
 
+public class Resolver implements Expr.Visitor<Void>,Stmt.Visitor<Void>{
+	private final Interpreter interpreter;
+	private final Stack<Map<String,Boolean>> scopes= new Stack<>();
+	
+	Resolver(Interpreter interpreter){
+		this.interpreter=interpreter;
+	}
+	void resolve(List<Stmt> statements) {
+		for(Stmt statement:statements) {
+			resolve(statement);
+		}
+	}
+	@Override
+	public Void visitBlockStmt(Stmt.Block stmt) {
+		beginScope();
+		resolve(stmt.statements);
+		endScope();
+		return null;
+	}
+	@Override
+	public Void visitVarStmt(Stmt.Var stmt) {
+		declare(stmt.name);
+		if(stmt.initializer!=null) {
+			resolve(stmt.initializer);
+		}
+		define(stmt.name);
+		return null;
+	}
+	private void resolve(Stmt stmt) {
+		stmt.accept(this);
+	}
+	private void resolve(Expr expr) {
+		expr.accept(this);
+	}
+	private void beginScope() {
+		scopes.push(new HashMap<String,Boolean>());
+	}
+	private void endScope() {
+		scopes.pop();
+	}
 }
